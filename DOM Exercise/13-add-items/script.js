05383dafaa4274f2022b5e0f1c67bb29f7c6d3bd
napostolova <napostolova01@gmail.com>
2024-07-11 21:51:35 +0300

    Add logic to create new item

--- DOM Exercise/13-add-items/script.js
+++ DOM Exercise/13-add-items/script.js
@@ -1,3 +1,10 @@
-function creatNew() {
-    
+function createItem() {
+    let items = document.querySelector('#items');
+    let newItem = document.createElement('li');
+
+    let itemOutput = document.querySelector('input[name=item]');
+    newItem.innerHTML = itemOutput.value;
+    items.appendChild(newItem);
+
+    itemOutput.value = "";
 }
